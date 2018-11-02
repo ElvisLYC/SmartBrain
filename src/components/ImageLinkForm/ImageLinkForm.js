7acf4a16e698ebb0f04d82c68012aef583c5f6fa
Elvis <yinchuang94@gmail.com>
2018-11-02 15:57:53 +0800

    added image box and clarifai api

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './ImageLinkForm.css'
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
   return(
-    <div className = 'ma4 mt0'>
+    <div className = 'ma4 mt0 mb0'>
       <p className='f3'>
         {'This SmartBrain will detect faces in your pictures. Come on and give it a try!'}
       </p>
       <div className='center'>
         <div className='center form pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='tex' />
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange} />
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
         </div>
       </div>
     </div>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
     <div className = 'ma4 mt0'>
       <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
         <div className="Tilt-inner">
-          <img style= {{paddingTop: '10px'}} alt='logo' src={smart}/> 
+          <img style= {{paddingTop: '10px'}} alt='logo' src={smart}/>
         </div>
       </Tilt>
     </div>
